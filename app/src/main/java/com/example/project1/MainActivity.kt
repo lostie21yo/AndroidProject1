ef714ed08b1cd915d750d1cf49d4779980ed50c0
lostie21yo <leon.coxsw@gmail.com>
2022-10-31 18:31:26 +0400

    Daily commit

--- app/src/main/java/com/example/project1/MainActivity.kt
+++ app/src/main/java/com/example/project1/MainActivity.kt
@@ -2,10 +2,44 @@ package com.example.project1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("MyLogMAct", "onCreate")
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("MyLogMAct", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("MyLogMAct", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("MyLogMAct", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("MyLogMAct", "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("MyLogMAct", "onDestroy")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("MyLogMAct", "onRestart")
+    }
+
 }
