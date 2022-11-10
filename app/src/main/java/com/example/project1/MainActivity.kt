a52e957d6694dccdfbe3431f12d84707a685a59a
lostie21yo <leon.coxsw@gmail.com>
2022-11-10 21:18:15 +0300

    lessons 8, 9

--- app/src/main/java/com/example/project1/MainActivity.kt
+++ app/src/main/java/com/example/project1/MainActivity.kt
@@ -6,32 +6,29 @@ import android.os.PersistableBundle
 import android.util.Log
 import android.view.View
 import android.widget.TextView
+import com.example.project1.databinding.ActivityMainBinding
 import kotlin.math.pow
 
 class MainActivity : AppCompatActivity() {
-
-    val const:Double = 2.0 // неизменяямая константа
-    var counter:Int = 0 // изменяемая переменная
-    var res:Double = 0.0
-    var bool:Boolean = "Привет, всему миру!".endsWith("миру!!")
-    lateinit var tv : TextView // var tv : TextView? = null
+    lateinit var bindingClass : ActivityMainBinding
+    val a : Float = 5.0f
+    val b : Float = 64.0f
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        tv = findViewById(R.id.tvTest)
-        Log.d("TestLog", bool.toString())
+        bindingClass = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bindingClass.root)
+        bindingClass.tv.text = "$a и $b"
+
+        bindingClass.b1.setOnClickListener{
+            bindingClass.tv.text = (a + b).toString()
+        }
+        bindingClass.b2.setOnClickListener{
+            bindingClass.tv.text = (a - b).toString()
+        }
+        bindingClass.b3.setOnClickListener{
+            bindingClass.tv.text = (a * b).toString()
+        }
     }
 
-    fun onClickTest(view: View){
-        counter += 1
-        res = const.pow(counter)
-        tv.text = res.toString()
-    }
-
-    fun onClickErase(view: View) {
-        res = 0.0
-        counter = 0
-        tv.text = "Erased!"
-    }
 }
